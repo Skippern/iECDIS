cd82d5957705c94eedbae53ea13ca0698ccaf035
Aun Johnsen <lists@gimnechiske.org>
2016-02-11 20:27:59 -0200

    Comment describing file purpose

--- iECDIS/Protocol/NMEA2000.m
+++ iECDIS/Protocol/NMEA2000.m
@@ -5,6 +5,8 @@
 //  Created by Aun Johnsen on 2/10/16.
 //  Copyright © 2016 Aun Johnsen. All rights reserved.
 //
+// This file contains rules for how to evaluate the content of NMEA2000 messages, what each telegram type represents, and what each segment value means
+//
 
 #import "NMEA2000.h"
 
